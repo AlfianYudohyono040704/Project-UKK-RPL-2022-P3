6470d8443335b859eda5b7ce7dfca4ca38adc3f5
AlfianYudohyono040704 <alfianyudohyono4189@gmail.com>
2022-03-27 21:03:11 +0700

    projectukk

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\User;
-use App\Models\ActivityLog;
 
 class HomeController extends Controller
 {
@@ -25,9 +23,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $user = User::select()->count();
-        $activity_log = ActivityLog::with('user')->latest()->limit(10)->orderBy('id','DESC')->get();
-
-        return view('home', compact('user', 'activity_log'));
+        return view('home');
     }
 }
